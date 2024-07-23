cbcfd4403314e0d84df8e9900bb9189ef177e829
Andrey Nikulochkin <nikulochkin.a@yandex.ru>
2024-07-23 22:53:39 +0300

    Добавил top-bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Container, Title, TopBar } from '@/components/shared';
 
-export default function page() {
-  return <div>50 минут 40 сек</div>;
+export default function Home() {
+  return (
+    <>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+      <TopBar />
+    </>
+  );
 }
---
+++ components/shared/top-bar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { cn } from '@/lib/utils';
+import { Categories, SortPopup, Container } from '@/components/shared';
+interface Props {
+  className?: string;
+}
+
+export const TopBar: React.FC<Props> = ({ className }) => {
+  return (
+    <div className={cn('sticky top-0 bg-white py-5 shadow-lg shadow-black/5 z10', className)}>
+      <Container className="flex items-center justify-between">
+        <Categories />
+        <SortPopup />
+      </Container>
+    </div>
+  );
+};
