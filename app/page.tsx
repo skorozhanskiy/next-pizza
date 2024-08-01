aa51e7e27dd4e708845034676039b081119a2f09
Andrey Nikulochkin <nikulochkin.a@yandex.ru>
2024-08-01 12:15:35 +0300

    добавил блок фильтрации

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Title, TopBar } from '@/components/shared';
+import { Container, Title, TopBar, Filters } from '@/components/shared';
 
 export default function Home() {
   return (
@@ -8,6 +8,14 @@ export default function Home() {
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
       </Container>
       <TopBar />
+      <Container className="mt-10 mb-14">
+        <div className="flex gap-[60px]">
+          {/*-фильтрация-*/}
+          <Filters />
+          {/* <ProductsGroopList title 'Пиццы' items={[1,2,3,4,5]} */}
+          Список товаров
+        </div>
+      </Container>
       <p>1:04</p>
     </>
   );
---
+++ components/shared/checkbox-filters-group.tsx
@@ -0,0 +1,73 @@
+'use client';
+import React from 'react';
+import { FilterChecboxProps } from './filter-checkbox';
+import { FilterCheckbox } from './filter-checkbox';
+import { Input } from '../ui/input';
+
+type Item = FilterChecboxProps;
+
+interface Props {
+  title: string;
+  items: Item[];
+  defaultItems: Item[];
+  limit?: number;
+  searchInputPlaceholder?: string;
+  onChange?: (value: string[]) => void;
+  defaultValue: string;
+  className?: string;
+}
+
+export const CheckboxFiltersGroup: React.FC<Props> = ({
+  title,
+  items,
+  defaultItems,
+  limit = 5,
+  searchInputPlaceholder = 'Поиск...',
+  defaultValue,
+  className,
+}) => {
+  const [showAll, setShowAll] = React.useState(false);
+  const [searchValue, setSearchValue] = React.useState('');
+  const list = showAll
+    ? items.filter((item) => item.text.toLowerCase().includes(searchValue.toLocaleLowerCase()))
+    : defaultItems.slice(0, limit);
+
+  const onChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+  return (
+    <div className={className}>
+      <p className="font-bold mb-3">{title}</p>
+      {showAll && (
+        <div className="mb-5">
+          <Input
+            onChange={onChangeSearchInput}
+            placeholder={searchInputPlaceholder}
+            className="bg-gray-50 border-none"
+          />
+        </div>
+      )}
+      <div className="flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar">
+        {list.map((item, index) => (
+          <FilterCheckbox
+            key={index}
+            text={item.text}
+            value={item.value}
+            endAdornment={item.endAdornment}
+            // checked={selected?.has(item.value)}
+            checked={false}
+            // onCheckedChange={() => onClickCheckbox?.(item.value)}
+            onCheckedChange={() => console.log(item.value)}
+          />
+        ))}
+      </div>
+      {items.length > limit && (
+        <div className={showAll ? 'border-t border-t-neutral-100 mt-4' : ''}>
+          <button className="text-primary mt-3" onClick={() => setShowAll(!showAll)}>
+            {showAll ? 'Скрыть' : '+ Показать все'}
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
